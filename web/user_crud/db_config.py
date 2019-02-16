d36089afab35c52623ecd3e53ca9ded171a4cfe8
Hosen Poursaeid <poursaeid@outlook.com>
2019-02-16 17:37:50 -0500

    python-endpoint

---
+++ web/user_crud/db_config.py
@@ -0,0 +1,11 @@
+from app import app
+from flaskext.mysql import MySQL
+
+mysql = MySQL()
+
+# MySQL Configuratinos
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Dooste,2004'
+app.config['MYSQL_DATABASE_DB'] = 'roytuts'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+mysql.init_app(app)
